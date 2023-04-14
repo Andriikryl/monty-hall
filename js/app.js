61d0c95ac4986fef81ed03ea790d873ade8d49af
Andriikryl <andreyk557@gmail.com>
2023-04-14 22:58:32 +0300

    feat[checkWin]

--- js/app.js
+++ js/app.js
@@ -1,35 +1,74 @@
 const doors = [];
 const state = "PICK";
+let pickedDoor;
+let revealedDoor;
 
 function setup() {
   noCanvas();
 
   for (let i = 0; i < 3; i++) {
     doors[i] = createDiv("");
+    doors[i].parent("#doors");
     doors[i].class("door");
     doors[i].prize = "goat";
 
     doors[i].index = i;
-    doors[i].mousePressed(reveal);
+    doors[i].mousePressed(pickDoor);
   }
 
   let winningDoor = random(doors);
   winningDoor.prize = "prize";
 }
 
-function reveal() {
-  console.log(this.index);
+function pickDoor() {
+  this.style("background-color", "#AFA");
+  pickedDoor = this;
+  reveal();
+}
 
+function reveal() {
   const options = [];
   for (let i = 0; i < doors.length; i++) {
     const door = doors[i];
-    if (i !== this.index && door.prize !== "prize") {
+    if (i !== pickedDoor.index && door.prize !== "prize") {
       options.push(door);
     }
   }
 
-  this.style("background-color", "#AFA");
+  revealedDoor = random(options);
+  revealedDoor.html(revealedDoor.prize);
 
-  const doorReveal = random(options);
-  doorReveal.html(doorReveal.prize);
+  const switchButton = createButton("switch");
+  switchButton.mousePressed(playerSwitch);
+
+  const stayButton = createButton("stay");
+  stayButton.mousePressed(checkWin);
+}
+
+function playerSwitch() {
+  let newPick;
+
+  for (let i = 0; i < doors.length; i++) {
+    let door = doors[i];
+    if (door !== pickDoor && door !== revealedDoor) {
+      newPick = door;
+      break;
+    }
+  }
+  pickDoor = newPick;
+  checkWin();
+}
+
+function checkWin() {
+  if (pickedDoor.prize == "prize") {
+    createP("You win!");
+    pickedDoor.style("background-color", "#AFA");
+  } else {
+    createP("You lose!");
+    pickedDoor.style("background-color", "#FAA");
+  }
+
+  for (let door of doors) {
+    door.html(door.prize);
+  }
 }
