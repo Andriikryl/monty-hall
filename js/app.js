bbd8eb7da36029b8a2e9f96d674d14aeb8ba5d13
Andriikryl <andreyk557@gmail.com>
2023-04-14 22:15:36 +0300

    feat[options]

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 const doors = [];
+const state = "PICK";
 
 function setup() {
   noCanvas();
@@ -7,8 +8,28 @@ function setup() {
     doors[i] = createDiv("");
     doors[i].class("door");
     doors[i].prize = "goat";
+
+    doors[i].index = i;
+    doors[i].mousePressed(reveal);
   }
 
   let winningDoor = random(doors);
   winningDoor.prize = "prize";
+}
+
+function reveal() {
+  console.log(this.index);
+
+  const options = [];
+  for (let i = 0; i < doors.length; i++) {
+    const door = doors[i];
+    if (i !== this.index && door.prize !== "prize") {
+      options.push(door);
+    }
+  }
+
+  this.style("background-color", "#AFA");
+
+  const doorReveal = random(options);
+  doorReveal.html(doorReveal.prize);
 }
